1b2105afcfe6720709e77f979ce1b56fb2e86fd1
maxcaplan <maxacaplan@gmail.com>
2021-02-03 22:42:27 -0400

    basic player movment

---
+++ Assets/Scripts/Player/Controller2D.cs
@@ -0,0 +1,289 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller2D : RayCollisionController
+{
+    float maxClimbAngle = 80;
+    float MaxDescendAngle = 75;
+
+    public CollisionInfo collisions;
+
+    public Vector2 standingColliderSize = new Vector2(2, 2);
+    public Vector2 standingColliderPos = new Vector2();
+
+    public Vector2 crouchingColliderSize = new Vector2(2, 2);
+    public Vector2 crouchingColliderPos = new Vector2();
+
+    // Override inherited start function and run it in child
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Moves the player with input velocity
+    public void Move(Vector3 velocity, bool standingOnPlatform = false, bool ropeAttached = false)
+    {
+        UpdateRaycastOrigins();
+        collisions.Reset();
+        collisions.velocityOld = velocity;
+
+        if (ropeAttached) return;
+
+        if (velocity.y < 0)
+        {
+            DescendSlope(ref velocity);
+        }
+
+        // Check collisions
+        if (velocity.x != 0)
+        {
+            HorizontalCollisions(ref velocity);
+        }
+        if (velocity.y != 0)
+        {
+            VerticalCollisions(ref velocity);
+        }
+
+        // Move player
+        transform.Translate(velocity);
+
+        if (standingOnPlatform)
+        {
+            collisions.below = true;
+        }
+    }
+
+    // Calculate collisions for horizontal rays
+    void HorizontalCollisions(ref Vector3 velocity)
+    {
+        float directionX = Mathf.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            // Set ray origin to bottom or top based on velocity x direction
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            // Cast ray 
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+            //Draws horizontal rays for debugging
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            if (hit)
+            {
+                if (hit.distance == 0)
+                {
+                    continue;
+                }
+
+                // Offset velocity for sloped surfaces
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+                if (i == 0 && slopeAngle <= maxClimbAngle)
+                {
+                    if (collisions.descendingSlope)
+                    {
+                        collisions.descendingSlope = false;
+                        velocity = collisions.velocityOld;
+                    }
+
+                    float distanceToSlopeStart = 0;
+                    if (slopeAngle != collisions.slopeAngleOld)
+                    {
+                        distanceToSlopeStart = hit.distance - skinWidth;
+                        velocity.x -= distanceToSlopeStart * directionX;
+                    }
+
+                    ClimbSlope(ref velocity, slopeAngle);
+                    velocity.x += distanceToSlopeStart * directionX;
+                }
+
+                if (!collisions.climbingSlope || slopeAngle > maxClimbAngle)
+                {
+                    // Modify velocity to end at collision surface
+                    velocity.x = (hit.distance - skinWidth) * directionX;
+                    // Change raylength so as to only collide with closest surface
+                    rayLength = hit.distance;
+
+                    // Adjust y velocity if colliding with wall while climbing a slope
+                    if (collisions.climbingSlope)
+                    {
+                        velocity.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad * Mathf.Abs(velocity.x));
+                    }
+
+                    // Set which side of the player collided
+                    collisions.left = directionX == -1;
+                    collisions.right = directionX == 1;
+                }
+            }
+        }
+    }
+
+    // Calculate collisions for vertical rays
+    bool VerticalCollisions(ref Vector3 velocity)
+    {
+        bool collided = false;
+
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            // Set ray origin to bottom or top based on velocity y direction
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            // Cast ray 
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            //Draws vertical rays for debugging
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+            if (hit)
+            {
+                collided = true;
+
+                // Modify velocity to end at collision surface
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                // Change raylength so as to only collide with closest surface
+                rayLength = hit.distance;
+
+                if (collisions.climbingSlope)
+                {
+                    velocity.x = velocity.y / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(velocity.x);
+                }
+
+                // Set which side of the plauer collided
+                collisions.below = directionY == -1;
+                collisions.above = directionY == 1;
+            }
+        }
+
+        if (collisions.climbingSlope)
+        {
+            float directionX = Mathf.Sign(velocity.x);
+            rayLength = Mathf.Abs(velocity.x + skinWidth);
+            Vector2 rayOrigin = ((directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight) + Vector2.up * velocity.y;
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+            if (hit)
+            {
+                collided = true;
+
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+                if (slopeAngle != collisions.slopeAngle)
+                {
+                    velocity.x = (hit.distance - skinWidth) * directionX;
+                    collisions.slopeAngle = slopeAngle;
+                }
+            }
+        }
+
+        return collided;
+    }
+
+    // Calculates x y velocities to move the x distance of the input velocity on an upwards slope
+    void ClimbSlope(ref Vector3 velocity, float slopeAngle)
+    {
+        float moveDistance = Mathf.Abs(velocity.x);
+        float climbVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+
+        if (velocity.y <= climbVelocityY)
+        {
+            velocity.y = climbVelocityY;
+            velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+
+            collisions.below = true;
+            collisions.climbingSlope = true;
+            collisions.slopeAngle = slopeAngle;
+        }
+    }
+
+    void DescendSlope(ref Vector3 velocity)
+    {
+        float directionX = Mathf.Sign(velocity.x);
+        Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomRight : raycastOrigins.bottomLeft;
+
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down, Mathf.Infinity, collisionMask);
+
+        if (hit)
+        {
+            float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+            if (slopeAngle != 0 && slopeAngle <= MaxDescendAngle)
+            {
+                if (Mathf.Sign(hit.normal.x) == directionX)
+                {
+                    if (hit.distance - skinWidth <= Mathf.Tan(slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x))
+                    {
+                        float moveDistance = Mathf.Abs(velocity.x);
+                        float descendVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+
+                        velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+                        velocity.y -= descendVelocityY;
+
+                        collisions.below = true;
+                        collisions.slopeAngle = slopeAngle;
+                        collisions.descendingSlope = true;
+                    }
+                }
+            }
+        }
+    }
+
+    // Sets player hitbox for crouching and standing states
+    public void SetCrouchCollisions(bool crouching = false)
+    {
+        if (crouching)
+        {
+            collider2d.size = crouchingColliderSize;
+            collider2d.offset = crouchingColliderPos;
+        }
+        else
+        {
+            collider2d.size = standingColliderSize;
+            collider2d.offset = standingColliderPos;
+        }
+    }
+
+    // Checks wether the player can uncrouch
+    public bool CheckUncrouch(Vector3 velocity)
+    {
+        // Check if there is an obstacle less than the standing hitboxes height away
+        float crouchTop = crouchingColliderSize.y;
+        float standingTop = standingColliderSize.y;
+        velocity.y = Mathf.Abs(Mathf.Abs(standingTop) - Mathf.Abs(crouchTop)) - skinWidth * 2;
+        bool hit = VerticalCollisions(ref velocity);
+
+        if (hit)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public struct CollisionInfo
+    {
+        public bool above, below;
+        public bool left, right;
+
+        public bool climbingSlope, descendingSlope;
+        public float slopeAngle, slopeAngleOld;
+
+        public Vector3 velocityOld;
+
+        public void Reset()
+        {
+            above = below = false;
+            left = right = false;
+
+            climbingSlope = false;
+            descendingSlope = false;
+
+            slopeAngleOld = slopeAngle;
+            slopeAngle = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,282 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(LineRenderer))]
+public class Player : MonoBehaviour
+{
+    [Header("Jumping")]
+    public float maxJumpHeight = 2;
+    public float minJumpHeight = 1;
+    public float timeToJumpApex = .4f;
+
+    [Header("Move Speed")]
+    public float walkSpeed = 6;
+    public float crouchMoveSpeed = 3;
+
+    [Header("Acceleration")]
+    public float walkAccelerationAirborne = .2f;
+    public float walkAccelerationGrounded = .1f;
+    public float crouchAcceleration = .05f;
+
+    [Header("Deacceleration")]
+    public float walkDeaccelerationAirborne = .1f;
+    public float walkDeaccelerationGrounded = .05f;
+    public float crouchedDeacceleration = .05f;
+    public float movingToCrouchDeacceleration = 0.25f;
+    public float momentumDeacceleraionAirborne = 0.5f;
+    public float momentumDeacceleraionGrounded = 0.25f;
+
+    [Header("Aiming")]
+    public float minAimDistance = 1f;
+    public float maxAimDistance = 10f;
+    public Color canShootColor = Color.green;
+    public Color cantShootColor = Color.red;
+    public LayerMask grappleLayer;
+
+    [Header("Grappling")]
+    public float retractionSpeed = 0.1f;
+    public float extensionSpeed = 0.1f;
+
+    [Header("Game Objects")]
+    public GameObject aimPivot;
+    public GameObject aimRetical;
+    public GameObject aimSpot;
+
+    float gravity;
+    float maxJumpVelocity;
+    float minJumpVelocity;
+
+    float targetVelocityX;
+    float velocityXSmoothing;
+    Vector3 velocity;
+    Vector3 swingVelocity;
+
+    public Vector3 _velocity
+    {
+        get
+        {
+            return velocity;
+        }
+    }
+
+    bool crouching = false;
+    bool previousCrouchState = false;
+
+    float aimRotation;
+    Vector2 aimDirection;
+    RaycastHit2D aimPosition;
+
+    bool canShoot = false;
+    bool ropeAttached = false;
+
+    float targetRopeDistance;
+
+    Controller2D controller;
+    SpriteRenderer sprite;
+    Animator animator;
+    LineRenderer rope;
+
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        rope = GetComponent<LineRenderer>();
+
+        rope.enabled = false;
+
+        // Calculate gravity and jump velocity
+        gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+    }
+
+    void Update()
+    {
+        // Reset y velocity on vertical collisions
+        if (controller.collisions.above || controller.collisions.below || ropeAttached) velocity.y = 0;
+
+        if (ropeAttached) velocity.x = 0;
+
+        Vector3 oldVelocity = velocity;
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        // Calculate x y velocity for inputs
+        CalculateJump();
+        CalculateXVelocity(input);
+
+        // Set crouching state
+        UpdateCrouchState(input, velocity);
+
+        // Add gravity to velocity
+        velocity.y += gravity * Time.deltaTime;
+
+        // Move player
+        controller.Move(velocity * Time.deltaTime, false, ropeAttached);
+
+        // Handle grappling
+        UpdateAim();
+        UpdateGrapple(oldVelocity);
+        UpdateGrappleMovment(input);
+
+        // Update Aiming
+        CalculateAimDirection();
+        aimPivot.transform.eulerAngles = new Vector3(aimPivot.transform.eulerAngles.x, aimPivot.transform.eulerAngles.y, aimRotation);
+
+        // Update Animation
+        UpdateAnimation(input);
+
+        aimRetical.SetActive(!ropeAttached);
+    }
+
+    void UpdateCrouchState(Vector2 input, Vector3 velocity)
+    {
+        previousCrouchState = crouching;
+        crouching = (ropeAttached) ? false : (input.y < -0.5 && controller.collisions.below);
+
+        // If player is uncrouching, check that there is room to.
+        if (previousCrouchState && !crouching)
+        {
+            crouching = controller.CheckUncrouch(velocity * Time.deltaTime);
+        }
+
+        controller.SetCrouchCollisions(crouching);
+
+        // // If crouch state changed, update raycast spacing
+        if (previousCrouchState != crouching)
+        {
+            controller.CalculateRaySpacing();
+        }
+    }
+
+    // Sets jump velocity based on input
+    void CalculateJump()
+    {
+        if (Input.GetButtonDown("Jump") && controller.collisions.below)
+        {
+            velocity.y = maxJumpVelocity;
+        }
+        if (Input.GetButtonUp("Jump") && velocity.y > minJumpVelocity)
+        {
+            velocity.y = minJumpVelocity;
+        }
+    }
+
+    // Sets x velocity based on input
+    void CalculateXVelocity(Vector2 input)
+    {
+        float accelerationTime;
+        float deaccelerationTime;
+        float smoothingTime;
+
+        if (!crouching)
+        {
+            targetVelocityX = input.x * walkSpeed;
+            accelerationTime = (controller.collisions.below) ? walkAccelerationGrounded : walkAccelerationAirborne;
+            deaccelerationTime = (controller.collisions.below) ? walkDeaccelerationGrounded : walkDeaccelerationAirborne;
+            smoothingTime = (targetVelocityX != 0) ? accelerationTime : deaccelerationTime;
+        }
+        else
+        {
+            targetVelocityX = input.x * crouchMoveSpeed;
+            accelerationTime = crouchAcceleration;
+            deaccelerationTime = crouchedDeacceleration;
+            smoothingTime = (targetVelocityX != 0) ? accelerationTime : deaccelerationTime;
+            smoothingTime = (Mathf.Abs(targetVelocityX) < Mathf.Abs(velocity.x)) ? movingToCrouchDeacceleration : smoothingTime;
+        }
+
+        if (input.x != 0 && Mathf.Abs(velocity.x) > Mathf.Abs(targetVelocityX) && Mathf.Abs(velocity.x) > walkSpeed)
+        {
+            smoothingTime = (controller.collisions.below) ? momentumDeacceleraionGrounded : momentumDeacceleraionAirborne;
+        }
+
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, smoothingTime);
+    }
+
+    // Calculates direction and angle between aim pivot and mouse
+    void CalculateAimDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+
+        aimDirection = worldPosition - (Vector2)transform.position;
+        aimRotation = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+    }
+
+    void UpdateAim()
+    {
+        if (ropeAttached) return;
+
+        RaycastHit2D hit = Physics2D.Raycast(aimPivot.transform.position, aimDirection.normalized, maxAimDistance, grappleLayer);
+        Debug.DrawRay(aimPivot.transform.position, aimDirection.normalized * maxAimDistance, Color.green);
+
+        if (hit)
+        {
+            canShoot = (hit.distance > minAimDistance);
+            aimPosition = hit;
+
+            aimSpot.transform.position = hit.point;
+            aimSpot.GetComponent<SpriteRenderer>().color = (canShoot) ? canShootColor : cantShootColor;
+            aimSpot.SetActive(true);
+        }
+        else
+        {
+            aimSpot.SetActive(false);
+            canShoot = false;
+        }
+    }
+
+    void UpdateGrapple(Vector3 oldVelocity)
+    {
+        if (Input.GetButton("Fire1") && canShoot)
+        {
+            // Enable and update rope rendering
+            rope.enabled = true;
+            rope.SetPosition(0, aimPivot.transform.position);
+            rope.SetPosition(1, aimPosition.point);
+
+            Vector2 ropeDir = (aimPosition.point - (Vector2)aimPivot.transform.position).normalized;
+            Vector2 perpendicularVelocity = Vector2.Perpendicular(ropeDir);
+
+            float speed = oldVelocity.magnitude;
+            perpendicularVelocity *= -Mathf.Sign(oldVelocity.x);
+
+            Debug.DrawRay(aimPivot.transform.position, perpendicularVelocity * -Mathf.Sign(swingVelocity.x), Color.yellow);
+
+            ropeAttached = true;
+
+        }
+        else
+        {
+            // Disable rope and physics
+            rope.enabled = false;
+
+            // Update plauers velocity when leaving rope
+            // if (ropeAttached) velocity = swingVelocity;
+
+            ropeAttached = false;
+        }
+    }
+
+    void UpdateGrappleMovment(Vector2 input)
+    {
+    }
+
+    void UpdateAnimation(Vector2 input)
+    {
+        if (input.x != 0)
+        {
+            sprite.flipX = (Mathf.Sign(input.x) == -1);
+        }
+        animator.SetFloat("velocityX", Mathf.Abs(targetVelocityX));
+        animator.SetFloat("velocityY", velocity.y);
+        animator.SetBool("grounded", controller.collisions.below);
+        animator.SetBool("crouching", crouching);
+    }
+}
